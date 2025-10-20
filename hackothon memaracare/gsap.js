9640496fa52553598c4a2e42f1880ed8dfcd5c4b
suryakiranlakkireddy <suryakiranlakkireddy@gmail.com>
2025-10-20 11:04:52 +0530

    Add files via upload

---
+++ hackothon memaracare/gsap.js
@@ -0,0 +1,33 @@
+gsap.from(".k",{
+    y:20,
+    opacity:0,
+   
+    duration:0.5,
+    delay:0.5,
+    stagger:0.5
+})
+gsap.from(".t",{
+    y:20,
+    opacity:0,
+   
+    duration:0.5,
+    delay:0.5,
+    stagger:0.5
+})
+gsap.from(".p1",{
+    y:20,
+    opacity:0,
+   scale:0.5,
+    duration:0.5,
+    delay:0.5,
+    stagger:0.5
+})
+gsap.from(".pp1",{
+    y:-200,
+    opacity:0,
+   scale:0.5,
+    duration:1,
+    delay:0.5,
+    stagger:0.5
+    
+})
